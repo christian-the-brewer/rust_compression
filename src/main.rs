fb6f6329837e527800aa6509f53aa306e622947f
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-20 22:21:08 -0500

    sets up imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main() {}
