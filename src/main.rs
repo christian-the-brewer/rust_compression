4da315738cced6088556f260e0cb17088ba954db
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-21 08:27:57 -0500

    cleanso up code and comments

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::io::BufReader;
 use std::time::Instant;
 
 fn main() {
+    //check for required args
     if args().len() != 3 {
         eprintln!("Usage: 'source' 'target'");
         return;
@@ -16,20 +17,19 @@ fn main() {
     //take in file and destination for encoding
     let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
     let output = File::create(args().nth(2).unwrap()).unwrap();
-
+    //encoder
     let mut encoder = GzEncoder::new(output, Compression::default());
-
+    //for tracking time
     let start = Instant::now();
-
+    //encoding input with encoder
     copy(&mut input, &mut encoder).unwrap();
+    //saving as output
     let output = encoder.finish().unwrap();
-
+    //print info to user
     println!(
-        "Source len: {:?}",
+        "Uncompressed size: {:?}",
         input.get_ref().metadata().unwrap().len()
     );
-
-    println!("Compressed len: {:?}", output.metadata().unwrap().len());
-
+    println!("Compressed size: {:?}", output.metadata().unwrap().len());
     println!("Time elapsed: {:?}", start.elapsed());
 }
