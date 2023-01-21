ef7dfb0aecbf39a689333c404d31ca46f17ccb08
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-20 22:53:42 -0500

    complete project

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,28 @@ use std::io::copy;
 use std::io::BufReader;
 use std::time::Instant;
 
-fn main() {}
+fn main() {
+    if args().len() != 3 {
+        eprintln!("Usage: 'source' 'target'");
+        return;
+    }
+    //take in file and destination for encoding
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+
+    let mut encoder = GzEncoder::new(output, Compression::default());
+
+    let start = Instant::now();
+
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+
+    println!(
+        "Source len: {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+
+    println!("Compressed len: {:?}", output.metadata().unwrap().len());
+
+    println!("Time elapsed: {:?}", start.elapsed());
+}
